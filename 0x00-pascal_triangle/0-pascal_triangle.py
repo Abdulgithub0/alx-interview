eb49ac66ef5232577b94f3e065073967f81b9b81
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-02 23:44:49 +0100

    function to compute pascal traingle of N height

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
 """
-Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
-    - Returns an empty list if n <= 0
-    - You can assume n will be always an integer
+- Create a function def pascal_triangle(n):
+    that returns a list of lists of integers representing
+    the Pascal’s triangle of n:
+- Returns an empty list if n <= 0
+- You can assume n will be always an integer
 """
 
 
 def pascal_triangle(n: int = None) -> list[list[int]]:
-    """compute a 2D list of elements representing pascal triangle for N height"""
+    """compute 2D list of elements representing pascal triangle for N height"""
 
-    if ( not n or n <= 0): return []
+    if (not n or n <= 0):
+        return []
     grid: list[list[int]] = [[1], [1, 1]]
 
     for i in range(2, n):
