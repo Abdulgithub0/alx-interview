962954cdf040deb9bba3d171d3f40f85e860fa91
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-02 23:57:44 +0100

    function to compute pascal triangle of N height

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""Create a function def pascal_triangle(n):
-   that returns a list of lists of integers representing
-   the Pascal’s triangle of n:
-        - Returns an empty list if n <= 0
-        - You can assume n will be always an integer
 """
-
+Create a function def pascal_triangle(n):
+that returns a list of lists of integers representing
+the Pascal’s triangle of n:
+    - Returns an empty list if n <= 0
+    - You can assume n will always be an integer
+"""
 
 def pascal_triangle(n: int = None) -> list[list[int]]:
     """
-    compute 2D list of elements representing pascal triangle for N height
+    Compute a 2D list of elements representing Pascal's triangle for N.
+    
+    :param n: The height of Pascal's triangle.
+    :type n: int
+    :return: A list of lists representing Pascal's triangle.
+    :rtype: list[list[int]]
     """
-
-    """check if argument meet the specified requirement"""
-    if (not n or n <= 0):
+    if not n or n <= 0:
         return []
 
-    """create default pascal triangle value"""
     grid: list[list[int]] = [[1]]
 
     for i in range(1, n):
-        """add new entry to the grid"""
         new: list[int] = [1]
 
         for j in range(1, i):
             new.append(grid[i - 1][j] + grid[i - 1][j - 1])
+
         new.append(1)
         grid.append(new)
+
     return grid
