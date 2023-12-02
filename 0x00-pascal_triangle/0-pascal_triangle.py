64d4446b47e46e3e00a246671c77819790ac9c7d
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-02 23:52:17 +0100

    function to compute pascal triangle of N height

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,14 +10,21 @@
 
 
 def pascal_triangle(n: int = None) -> list[list[int]]:
-    """compute 2D list of elements representing pascal triangle for N height"""
+    """
+    compute 2D list of elements representing pascal triangle for N height
+    """
 
+    """check if argument meet the specified requirement"""
     if (not n or n <= 0):
         return []
-    grid: list[list[int]] = [[1], [1, 1]]
 
-    for i in range(2, n):
+    """create default pascal triangle value"""
+    grid: list[list[int]] = [[1]]
+
+    for i in range(1, n):
+        """add new entry to the grid"""
         new: list[int] = [1]
+
         for j in range(1, i):
             new.append(grid[i - 1][j] + grid[i - 1][j - 1])
         new.append(1)
