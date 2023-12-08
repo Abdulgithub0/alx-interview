e3489f3ec07dd278987ac5c09b1e9ecb331a9d6b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-08 10:55:39 +0100

    solution to the lockboxes problem

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -21,3 +21,5 @@ def canUnlockAll(boxes):
                     if key not in keys_store:
                         to_be_open_boxes.add(key)
     return len(keys_store) == len(boxes)
+
+#print(canUnlockAll([ [10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [], [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6], [9, 5, 8, 8], [6, 2, 8, 6] ]))
