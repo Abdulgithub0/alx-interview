7851a178c716ae4069119ce7bfbd0ac579ba612c
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-04 12:08:43 +0100

    a function that determine if a character is a valid utf-8 1 byte character

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that determines if a given data
+    set represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """Return true if data contains
+       valid utf-8 characters or false otherwise
+       It assume each character in data parameter is 1 byte(ASCII)
+    """
+    for char in data:
+        if char >> 7 == 0:
+            continue
+        else:
+            return False
+    return True
