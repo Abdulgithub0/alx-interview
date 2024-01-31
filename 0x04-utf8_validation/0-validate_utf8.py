a336d3b0a300c1a88f6c343333dee1d0a6f51ae8
Abdulgithub0 <belloabdulsamad26@gmail.com>
2024-01-31 10:47:43 +0100

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,9 +10,20 @@ def validUTF8(data):
        It assume each element in data is 1 byte and only handle the 8 least
        significant bits of each element in data
     """
-    for char in data:
-        if char >> 7 == 0:
-            continue
+
+    num_bytes = 0
+    for byte in data:
+        if num_bytes == 0:
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 7 != 0:
+                return False
         else:
-            return False
-    return True
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
